107689c7890e0de6dcbb935faed3274da6e3d748
dudu-duck <eduardo._correa@outlook.com>
2025-04-25 01:17:58 -0300

    ◉ Aplicação de mudanças guardadas em `stash` no `AppComponent`.
    - As mudanças realizadas no `AppComponent`  estavam guardadas em stash e foram aplicadas novamente.
    - Dentre as mudanças aplicadas, temos: a inclusão da navbar e do background animado.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { WorkComponent } from './pages/work/work.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        pathMatch: 'full',
+        component: WorkComponent
+    },
+    {
+        path: 'about-me',
+        pathMatch: 'full',
+        component: AboutMeComponent
+    }
+];
